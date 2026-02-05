ae700949ac94b4505e0ce3bfe169fd7c56b35cff
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2026-02-05 23:49:07 +0530

    Kids With the Greatest Number of Candies

---
+++ Array/GreatestNumberCandies.java
@@ -0,0 +1,31 @@
+//
+//
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class GreatestNumberCandies {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+        System.out.println(kidsWithCandies(candies,extraCandies));
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+        List<Boolean> list = new ArrayList<>(candies.length);
+        for(int i = 0;i<candies.length; i++){
+            if(max<candies[i]){
+                max = candies[i];
+            }
+        }
+        for(int i =0;i<candies.length;i++){
+            if(candies[i]+extraCandies >= max){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
